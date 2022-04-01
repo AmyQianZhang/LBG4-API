8f89f2b828cf67258b59f0bebc62a06e83b57afe
AmyQianZhang <102151473+AmyQianZhang@users.noreply.github.com>
2022-04-01 15:59:03 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -2,18 +2,18 @@
 
 image=my-image
 container=my-container
-status=$(docker ps -a | grep $container)
+status=$(docker ps -a | grep ${container})
 
 if [ $status ]
 then
-	docker rm -f $container
+	docker rm -f ${container}
 fi
 
 npm i
 
 npm test
 
-docker build -t $image .
+docker build -t ${image} .
 docker image prune -f
 
-docker run -d -p 5000:5000 --name $container $image
+docker run -d -p 5000:5000 --name ${container} ${image}
