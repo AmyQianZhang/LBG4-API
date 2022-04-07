27281309600ab0a27e19749fcaed927cb8700ac9
AmyQianZhang <102151473+AmyQianZhang@users.noreply.github.com>
2022-04-07 16:36:44 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -17,7 +17,4 @@ docker image prune -f
 
 #docker run -d -p 5000:5000 --name ${container} ${image}
 
-gcloud auth activate-service-account --key-file ${SERVICE}
-gcloud auth configure-docker
-
 docker push gcr.io/lbg-210322/${image}
