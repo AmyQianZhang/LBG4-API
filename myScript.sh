633e703d930bf86f9eda9aba6860455a5f74eb38
AmyQianZhang <102151473+AmyQianZhang@users.noreply.github.com>
2022-04-07 16:41:16 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-image=my-image
-container=my-container
+image=amy-image
+container=amy-container
 
 if docker ps -a | grep ${container} > /dev/null
 then
@@ -12,9 +12,9 @@ npm i
 
 npm test
 
-docker build -t gcr.io/lbg-210322/${image} .
+docker build -t gcr.io/lbg-210322/${image}:${BUILD_ID} .
 docker image prune -f
 
 #docker run -d -p 5000:5000 --name ${container} ${image}
 
-docker push gcr.io/lbg-210322/${image}
+docker push gcr.io/lbg-210322/${image}:${BUILD_ID}
