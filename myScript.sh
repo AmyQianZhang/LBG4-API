a26cab5d861164712121d6900a300bd0a0d308ae
AmyQianZhang <102151473+AmyQianZhang@users.noreply.github.com>
2022-04-07 16:22:17 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -12,19 +12,9 @@ npm i
 
 npm test
 
-docker build -t ${image} .
+docker build -t gcr.io/lbg-210322/${image} .
 docker image prune -f
 
 #docker run -d -p 5000:5000 --name ${container} ${image}
-
-docker tag ${image} gcr.io/lbg-210322/${image}
-
-sudo su - jenkins
-
-gcloud auth activiate-service-account --key-file=${service}
-
-gcloud config set account amy-jenkins@lbg-210322.iam.gserviceaccount.com
-
-gcloud auth configure-docker
 
 docker push gcr.io/lbg-210322/${image}
