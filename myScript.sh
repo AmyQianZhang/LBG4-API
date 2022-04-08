6a26ec3c8321f93fcd0086b66b24da136f63933c
AmyQianZhang <102151473+AmyQianZhang@users.noreply.github.com>
2022-04-08 12:42:11 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -24,6 +24,8 @@ docker push gcr.io/lbg-210322/${image}:latest
 
 kubectl apply -f kubernetes/application.yml
 
+kubectl rollout restart deployment amy-app-deploy
+
 #sed -e "s,{{VERSION}},${BUILD_ID},g" kubernetes/application.yml | kubectl apply -f -
 
 if ! kubectl get services amy-app-service > /dev/null
